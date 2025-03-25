c44b3a20252c548bc1baa4f7efb23e1ddb9da3bc
Chandra-febryan <saintdecyan@gmail.com>
2025-03-25 23:22:36 +0700

    ujicoba chart melalui database

---
+++ chart/char_dasar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <canvas id="myChart" style="position: relative; height: 50vh; width: 75vw;"></canvas>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni'],
+        datasets: [{
+            label: 'Penjualan',
+            data: [10,20,30,40,50,60],
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderWidth: 2
+        }]
+    }
+});
+   </script>
+</html>
---
+++ chart/kostumisasi.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kostumisasi Grafik</title>
+</head>
+<body>
+    <h2>Grafik Kostumisasi</h2>
+<canvas id="myChart" style="position: relative; height: 50vh; width: 75vw;"></canvas>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni'],
+        datasets: [{
+            label: 'Penjualan',
+            data: [10,20,30,40,50,60],
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderWidth: 2
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            tooltip: {
+                enabled: true,
+            }
+        },
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: "Bulan",
+                },
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: "Jumlah Penjualan",
+                },
+            },
+        },
+    },
+});
+   </script>
+</html>
